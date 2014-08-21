cf3ddfbdf8480210928c927716ccb281ee3deea5
Susan Prestage <susan.prestage@gmail.com>
2014-08-21 12:11:53 -0700

    First Gigasecond solution.

---
+++ gigasecond/gigasecond.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/env ruby
+require 'date'
+require 'time'
+GIGASECOND = 1_000_000_000
+ONE_DAY = 1*60*60*24
+
+class Gigasecond
+  def self.from(birth_date)
+    @giga_anniversary = birth_date
+    @giga_days = GIGASECOND / ONE_DAY
+
+    @giga_anniversary = @giga_anniversary + @giga_days
+
+    return @giga_anniversary
+  end
+end
+
---
+++ gigasecond/gigasecond_test.rb
@@ -0,0 +1,27 @@
+require 'minitest/autorun'
+require_relative 'gigasecond'
+
+class GigasecondTest < MiniTest::Unit::TestCase
+
+  def test_1
+    gs = Gigasecond.from(Date.new(2011, 4, 25))
+    assert_equal Date.new(2043, 1, 1), gs
+  end
+
+  def test_2
+    gs = Gigasecond.from(Date.new(1977, 6, 13))
+    assert_equal Date.new(2009, 2, 19), gs
+  end
+
+  def test_3
+    gs = Gigasecond.from(Date.new(1959, 7, 19))
+    assert_equal Date.new(1991, 3, 27), gs
+  end
+
+  def test_yourself
+    your_birthday = Date.new(2009, 1, 27)
+    gs = Gigasecond.from(your_birthday)
+    assert_equal Date.new(2040, 10, 5), gs
+  end
+
+end
